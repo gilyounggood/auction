c185c8ecaa1d290e619f683a12d8a1e33b9b6102
gilyounggood <https://github.com/gilyounggood/gamemarket.git>
2022-07-25 16:54:21 +0900

    결제 모달창 디자인 완료

--- front/src/components/modals/PointModal.js
+++ front/src/components/modals/PointModal.js
@@ -15,39 +15,58 @@ const Modal = styled.div`
 `
 
 const Modaltitle = styled.div`
+    width: 100%;
+    text-align: center;
     font-size: 1.6rem;
     color: #8e44ad;
     border-bottom: 1px solid gray;
-    padding: 20px;
-    transform: translate(0%, -210%);
+    padding: 15px;
+    transform: translate(0%, -70%);
 `
 
 const ModalBody = styled.div`
     width: 100%;
     font-weight: bold;
-    text-align: center;
     font-size: 1.2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column
+`
+
+const ModalSubTitle = styled.div`
+    width: 15rem;
+    text-align: left;
+    margin-bottom: 5px;
 `
 
 const ModalBottom = styled.div`
     top: 100%;
     left: 100%;
-    transform: translate(60%, 500%);
+    transform: translate(60%, 130%);
 `
 
 const ModalInput = styled.input`
     width: 50%;
     height: 27px;
-    font-size: 1.0rem
+    font-size: 1.0rem;
+    margin-bottom: 10px;
+`
+
+const ModalSelect = styled.select`
+    width: 52%;
+    height: 33px;
+    font-size: 1.0rem;
+    margin-bottom: 10px;
 `
 
 const PointModal = (props) => {
 
     const {open ,close} = props;
 
-    const pay = () => {
+    const pay = (username, phone, email, pay) => {
         close()
-        Payment()
+        Payment(username, phone, email ,pay)
     }
 
   return (
@@ -56,7 +75,7 @@ const PointModal = (props) => {
           justifyContent: "center",
           alignItems: "center",
           borderRadius: '1rem',
-          minHeight:'30rem',
+          minHeight:'35rem',
           minWidth: '30rem',
           position: 'absolute',
           top: '50%',
@@ -66,12 +85,35 @@ const PointModal = (props) => {
         >
             <ModalBody>
                 <Modaltitle>포인트 충전하기</Modaltitle>
-                    충전금액: <ModalInput placeholder= '1원당 1포인트' />
+                <ModalSubTitle>
+                        구매자 이름:
+                </ModalSubTitle>
+                <ModalInput placeholder= '홍길동' />
+                <ModalSubTitle>
+                        구매자 전화번호:
+                </ModalSubTitle>
+                <ModalInput placeholder= '01012345678' />
+                <ModalSubTitle>
+                        구매자 이메일:
+                </ModalSubTitle>
+                <ModalInput placeholder= 'xxx@xxx.com' />
+                <ModalSubTitle>
+                        충전 금액:
+                </ModalSubTitle>
+                <ModalInput placeholder= '1원당 1포인트' />
+                <ModalSubTitle>
+                        결제 수단:
+                </ModalSubTitle>
+                <ModalSelect>
+                    <option value="" selected>결제 수단 선택</option>
+                    <option value="card">카드</option>
+                    <option value="vbank">계좌이체</option>
+                </ModalSelect>
             </ModalBody>
             <ModalBottom>
                 <Button
                 style={{width: '6rem', height: '2rem'}}
-                onClick={pay}
+                onClick={()=> {pay('홍길동', '01012345678', '123.123.com', 20000)}}
                 >
                   충전하기
                 </Button>
--- front/src/payment/Payment.js
+++ front/src/payment/Payment.js
@@ -1,23 +1,23 @@
 import React from 'react';
 
-const Payment = (effect, deps, pay) => {
+const Payment = (username, phone, email ,pay) => {
     const onClickPayment = () => {
         const { IMP } = window;
         IMP.init('imp62845076');
 
         const data = {
             pg: 'html5_inicis',
-            pay_method: 'card',
+            pay_method: 'vbank',
             merchant_uid: `mid_${new Date().getTime()}`,
-            name: '결제 테스트',
-            amount: '1',
+            name: '포인트 충전',
+            amount: pay,
             custom_data: {
                 name: '부가정보',
                 desc: '세부 부가정보',
             },
-            buyer_name: '홍길동',
-            buyer_tel: '01012345678',
-            buyer_email: '14279625@gmail.com',
+            buyer_name: username,
+            buyer_tel: phone,
+            buyer_email: email,
             buyer_addr: '구천면로 000-00',
             buyer_postalcode: '01234',
         };
