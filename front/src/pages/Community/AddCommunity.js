18ecbd9f3a03ced43b0aae288cbc62586c9d956f
gilyounggood <https://github.com/gilyounggood/gamemarket.git>
2022-08-06 18:43:03 +0900

    커뮤니티 텍스트 에디터 추가 및 반응형 스타일 추가 변경

--- front/src/pages/Community/AddCommunity.js
+++ front/src/pages/Community/AddCommunity.js
@@ -5,15 +5,38 @@ import { useParams, useHistory, Link } from 'react-router-dom';
 import ContentsWrapper from '../../components/elements/ContentWrapper';
 import Wrapper from '../../components/elements/Wrapper';
 import axios from 'axios';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import Title from '../../components/elements/Title';
 import Button from '../../components/elements/Button';
-import Input from '../../components/elements/Input';
 import Container from '../../components/elements/Container';
 import Content from '../../components/elements/Content';
 import SubTitle from '../../components/elements/SubTitle';
 import RightButtonContainer from '../../components/elements/RightButtonContainer';
 import Textarea from '../../components/elements/Textarea';
 import CenterButtonContainer from '../../components/elements/CenterButtonContainer';
+import '../../styles/style.css'
+
+const Input = styled.input`
+    outline: 1px solid #c4c4c4;
+    font-size:1rem;
+    border:none;
+    margin-left:0.3rem;
+    width:39.2rem;
+    height: 2rem;
+    padding-bottom:0.2rem;
+    ::placeholder {
+        color: #cccccc;
+    }
+    @media screen and (max-width: 790px) {
+        width: 285px;
+    }
+    &:focus {
+        outline: 1px solid #0078FF;
+        box-shadow: 0px 0px 2px black;
+    }
+`
+
 const AddCommunity = () => {
     //----------------------------------------공통
     const history = useHistory()
@@ -73,9 +96,9 @@ const AddCommunity = () => {
             }
         }
     }
+
     return (
         <Wrapper >
-
             <ContentsWrapper style={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center'
                 , borderRadius: `${window.innerWidth >= 950 ? '1rem' : '0'}`
@@ -83,19 +106,40 @@ const AddCommunity = () => {
                 
                 <Title>{params.pk==1?'공지사항 추가':'자유게시판 추가'}</Title>
                 
-                <Container>
+                <Container style={{justifyContent: 'center', alignItems: 'center'}}>
                     <Content>
                         <SubTitle>제목</SubTitle>
                     </Content>
                     <Content>
-                        <Input type={'text'} onChange={onChangeTitle} />
+                        <Input 
+                            type={'text'} 
+                            onChange={onChangeTitle} 
+                        />
                     </Content>
                     <Content>
-                        <SubTitle>설명</SubTitle>
+                        <SubTitle>내용</SubTitle>
                     </Content>
-                    <Content>
+                    <CKEditor
+                        editor={ ClassicEditor }
+                        data=""
+                        onReady={ editor => {
+                            // You can store the "editor" and use when it is needed.
+                            console.log( 'Editor is ready to use!', editor );
+                        } }
+                        onChange={ ( event, editor ) => {
+                            const data = editor.getData();
+                            console.log( { event, editor, data } );
+                        } }
+                        onBlur={ ( event, editor ) => {
+                            console.log( 'Blur.', editor );
+                        } }
+                        onFocus={ ( event, editor ) => {
+                            console.log( 'Focus.', editor );
+                        } }
+                    />
+                    {/* <Content>
                         <Textarea onChange={onChangeNote} />
-                    </Content>
+                    </Content> */}
                 </Container>
                 <CenterButtonContainer>
                 <Button onClick={()=>{history.goBack()}}>
@@ -106,6 +150,9 @@ const AddCommunity = () => {
                     </Button>
                 </CenterButtonContainer>
             </ContentsWrapper>
+            {
+
+            }
         </Wrapper>
     );
 };
