630bf95dc874eb2ada2117ef75f3562c9f4f2178
gilyounggood <https://github.com/gilyounggood/gamemarket.git>
2022-08-28 16:23:38 +0900

    자동입력 방지숫자 구현

--- front/src/pages/Auth/ChangePw.js
+++ front/src/pages/Auth/ChangePw.js
@@ -5,6 +5,7 @@ import { useHistory, useParams } from 'react-router-dom';
 import ContentsWrapper from '../../components/elements/ContentWrapper';
 import Wrapper from '../../components/elements/Wrapper';
 import axios from 'axios';
+import { BsArrowClockwise } from 'react-icons/bs'
 
 const Input = styled.input`
 width:20.5rem;
@@ -21,7 +22,7 @@ margin-bottom:1rem;
     font-size:0.8rem;
   }
   ::placeholder {
-    color: #cccccc;
+    color: gray;
   }
   &:focus {
     border: 1px solid #0078FF;
@@ -66,6 +67,7 @@ const MyProfile = () => {
     const [newPw, setNewPw] = useState('')
     const [confirmNewPw, setConfirmNewPw] = useState('')
     const [number, setNumber] = useState(0);
+    const [userNumber, setUserNumber] = useState(0)
 
     async function fetchInfo(){
       const { data: response } = await axios.get('/api/auth')
@@ -89,11 +91,15 @@ const MyProfile = () => {
     }, [])
 
     const changePw = async () => {
-      if(!pw || !newPw || !confirmNewPw) {
+      if(!pw || !newPw || !confirmNewPw || !userNumber) {
         alert("모든 정보를 입력해주세요.");
         return;
       } else if (newPw !== confirmNewPw) {
         alert("새 비밀번호가 일치하지 않습니다");
+        return;
+      }
+      if(userNumber != number) {
+        alert("자동입력 방지숫자가 일치하지 않습니다.")
         return;
       }
       const { data: response } = await axios.post('/api/changepassword', {
@@ -119,10 +125,21 @@ const MyProfile = () => {
                 <SubTitle>새 비밀번호</SubTitle>
                 <Input style={{marginBottom:'15px'}} type='password' onChange={e => setNewPw(e.target.value)} />
                 <SubTitle>새 비밀번호 확인</SubTitle>
-                <Input style={{marginBottom:'20px'}} type='password' onChange={e => setConfirmNewPw(e.target.value)} />
+                <Input style={{marginBottom:'30px'}} type='password' onChange={e => setConfirmNewPw(e.target.value)} />
                 {newPw !== confirmNewPw &&
-                  <div style={{marginBottom:'2.5rem', color: 'red'}}>비밀번호가 일치하지 않습니다</div>
+                  <div style={{marginBottom:'20px', color: 'red'}}>비밀번호가 일치하지 않습니다</div>
                 }
+
+                <SubTitle style={{ marginBottom:'15px', fontSize: '0.8rem' }}>아래 숫자를 보이는 대로 입력해주세요</SubTitle>
+                <div style={{border: '1px solid black', padding: '1.5rem', marginBottom: '15px', color: '#464646', fontSize: '1.8rem'}}>
+                  <span style={{}}>{number}</span>
+                  <span 
+                    style={{ color: '#828282', fontSize: '1rem', fontWeight: 'bold', marginLeft: '10px', cursor: 'pointer' }}
+                    onClick={() => {randomNumber()}}
+                  ><BsArrowClockwise/>새로고침</span>
+                </div>
+                <Input style={{marginBottom:'15px'}} type='text' placeholder='자동입력 방지숫자' onChange={e => setUserNumber(e.target.value)}/>
+
                 <Button style={{marginBottom:'2rem'}}
                 onClick={() => {
                   if(window.confirm("비밀번호를 변경 하시겠습니까?")) {
